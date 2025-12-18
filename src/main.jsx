1919aa9360e32cefc706c8397c1509dc4f657ffc
re-fagiano <carlettidavidealessandro@gmail.com>
2025-12-18 16:58:11 +0100

    Fix app initialization by importing App component

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import App from './App'
 import './index.css'
 
 class RootErrorBoundary extends React.Component {
